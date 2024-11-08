fa57d75999b8ed438f027f7bdad797cf1f441047
Dogukan Kaan Bozkurt <dkaanbozkurt@gmail.com>
2024-11-08 13:31:10 +0100

    Use model.find() to Search Your Database

--- myApp.js
+++ myApp.js
@@ -35,8 +35,12 @@ const createManyPeople = (arrayOfPeople, done) => {
     })
 };
 
+// Use model.find() to Search Your Database
 const findPeopleByName = (personName, done) => {
-    done(null /*, data*/);
+    Person.find({ name: personName }, (err, data) => {
+        if(err) return console.error(err);
+        done(null, data);
+    })
 };
 
 const findOneByFood = (food, done) => {
