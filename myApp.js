7e7b17636ef3c803df7d781518622e80237c7a19
Dogukan Kaan Bozkurt <dkaanbozkurt@gmail.com>
2024-11-08 13:32:57 +0100

    Use model.findOne() to Return a Single Matching Document from Your Database

--- myApp.js
+++ myApp.js
@@ -43,8 +43,12 @@ const findPeopleByName = (personName, done) => {
     })
 };
 
+// Use model.findOne() to Return a Single Matching Document from Your Database
 const findOneByFood = (food, done) => {
-    done(null /*, data*/);
+    Person.findOne({favoriteFoods: food}, (err, data) => {
+        if(err) return console.error(err);
+        done(null, data);
+    });
 };
 
 const findPersonById = (personId, done) => {
