3ca7026e645b331c86a70221454d78574c152183
Dogukan Kaan Bozkurt <dkaanbozkurt@gmail.com>
2024-11-08 13:41:08 +0100

    Perform New Updates on a Document Using model.findOneAndUpdate()

--- myApp.js
+++ myApp.js
@@ -73,10 +73,14 @@ const findEditThenSave = (personId, done) => {
     });
 };
 
+// Perform New Updates on a Document Using model.findOneAndUpdate()
 const findAndUpdate = (personName, done) => {
     const ageToSet = 20;
 
-    done(null /*, data*/);
+    Person.findOneAndUpdate({name: personName}, {age: ageToSet}, {new: true}, (err, data) => {
+        if(err) return console.error(err);
+        done(null, data);
+    });
 };
 
 const removeById = (personId, done) => {
