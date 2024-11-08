a98c906665882ee2a7bbc5fb22051197e0480a4b
Dogukan Kaan Bozkurt <dkaanbozkurt@gmail.com>
2024-11-08 13:33:41 +0100

    Use model.findById() to Search Your Database By _id

--- myApp.js
+++ myApp.js
@@ -51,8 +51,12 @@ const findOneByFood = (food, done) => {
     });
 };
 
+// Use model.findById() to Search Your Database By _id
 const findPersonById = (personId, done) => {
-    done(null /*, data*/);
+    Person.findById(personId, (err, data) => {
+        if(err) return console.error(err);
+        done(null, data);
+    });
 };
 
 const findEditThenSave = (personId, done) => {
